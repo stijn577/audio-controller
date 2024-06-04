3d6bdbd5978a3085ff2f3fa5e0e49af64307bc13
stijn577 <stijnschotte03@gmail.com>
2024-06-04 04:13:00 +0200

    Basic stuff for controlling desktop audio is implemented
    Now the harder part, communication with the external hardware, TBC...

--- audio-controller-host/src/main.rs
+++ audio-controller-host/src/main.rs
@@ -1,17 +1,47 @@
-use wasapi::Direction;
+use std::time::Duration;
+
+use windows_volume_control::{AudioController, CoinitMode};
 
 fn main() {
-    wasapi::initialize_mta().ok();
-    wasapi::initialize_sta().ok();
+    loop {
+        std::thread::sleep(Duration::from_millis(1000));
 
-    let input = wasapi::DeviceCollection::new(&Direction::Render).unwrap();
-    let output = wasapi::DeviceCollection::new(&Direction::Capture).unwrap();
+        let audio_controller = controller_init();
+        let sessions = unsafe { audio_controller.get_all_session_names() };
 
-    input
-        .into_iter()
-        .for_each(|device| println!("{:?}", device.unwrap().get_friendlyname()));
+        let sessions = sessions
+            .into_iter()
+            .map(|name| unsafe { audio_controller.get_session_by_name(name) })
+            .collect::<Vec<_>>();
 
-    output
-        .into_iter()
-        .for_each(|device| println!("{:?}", device.unwrap().get_friendlyname()));
+        for session in sessions {
+            // TODO: read data from external hardware
+            match session {
+                Some(session) => unsafe { println!("{:?}", session.getName()) },
+                None => todo!(),
+            }
+        }
+
+        println!("🦀All processes listed! 🦀\n")
+    }
+}
+
+fn controller_init() -> AudioController {
+    let mut audiocontroller = unsafe { AudioController::init(None) };
+    audiocontroller.session_scan();
+    audiocontroller
+}
+
+trait MyAudioController {
+    fn session_scan(&mut self);
+}
+
+impl MyAudioController for AudioController {
+    fn session_scan(&mut self) {
+        unsafe {
+            self.GetSessions();
+            self.GetDefaultAudioEnpointVolumeControl();
+            self.GetAllProcessSessions();
+        }
+    }
 }
